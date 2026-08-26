eb6b491aada3224e2c602bebcc1e218b290feca1
itsmeakshaydighe-ui <itsmeakshaydighe@gmail.com>
2026-08-26 15:39:32 +0530

    debug: verbose npm log grep for lightningcss-linux resolution

--- apps/web/debug-install.sh
+++ apps/web/debug-install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-set -x
 rm -rf node_modules package-lock.json
-npm install --libc=glibc
-echo "--- DEBUG lightningcss ---"
-ls node_modules | grep lightningcss || echo "no lightningcss packages found"
+npm install --loglevel verbose 2>&1 | grep -i "lightningcss-linux" | head -30
+echo "--- exit code above was for grep, now real install ---"
+npm install 2>&1 | tail -5
